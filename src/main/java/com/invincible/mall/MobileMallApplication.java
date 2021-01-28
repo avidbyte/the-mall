b7208f274310aea5c058fb55393d9c287f166641
wudi <291100854@qq.com>
2021-01-28 17:05:18 +0800

    first commit

---
+++ src/main/java/com/invincible/mall/MobileMallApplication.java
@@ -0,0 +1,13 @@
+package com.invincible.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobileMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/invincible/mall/MobileMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invincible.mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
