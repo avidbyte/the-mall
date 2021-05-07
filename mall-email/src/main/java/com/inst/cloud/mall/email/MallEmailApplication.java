dd81e14e11042ccd579929a293756ee8f02bc7da
aaron <291100854@q.com>
2021-05-07 23:56:56 +0800

    add email module

---
+++ mall-email/src/main/java/com/inst/cloud/mall/email/MallEmailApplication.java
@@ -0,0 +1,16 @@
+package com.inst.cloud.mall.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author aaron
+ */
+@SpringBootApplication
+public class MallEmailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallEmailApplication.class, args);
+    }
+
+}
---
+++ mall-email/src/test/java/com/inst/cloud/mall/email/MallEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inst.cloud.mall.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallEmailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
